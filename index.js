7c87a99a51531795d6278e07c761b3a34a012121
Guicas <guilherme.pacheco2001@gmail.com>
2024-03-05 02:58:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'/public/*'))
 
 app.get('/login', function (req, res) {
     res.sendFile(path.join(__dirname+'/public/templates/login.html'))
