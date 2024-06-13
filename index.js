75d20369beebdff7ec167d291ca939ea7aa31a5a
Guicas <guilherme.pacheco2001@gmail.com>
2024-06-13 18:16:48 +0100

    99.9%

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 
 const app = express()
 const path = require('path')
@@ -6,25 +7,25 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'/landing_page.html'))
+    res.sendFile(path.join(__dirname+'/landing_page.html'));
 })
 app.get('/login', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/templates/login.html'))
+    res.sendFile(path.join(__dirname+'/public/templates/login.html'));
 })
 app.get('/contact', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/templates/contact.html'))
+    res.sendFile(path.join(__dirname+'/public/templates/contact.html'));
   })
 app.get('/menu', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/templates/menu.html'))
+    res.sendFile(path.join(__dirname+'/public/templates/menu.html'));
 })
 app.get('/create_account', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/templates/create_account.html'))
+    res.sendFile(path.join(__dirname+'/public/templates/create_account.html'));
 })
 app.get('/information', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/templates/information.html'))
+    res.sendFile(path.join(__dirname+'/public/templates/information.html'));
 })
 app.get('/save_info', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/templates/save_info.html'))
+    res.sendFile(path.join(__dirname+'/public/templates/save_info.html'));
 })
 
 app.listen(5500)
